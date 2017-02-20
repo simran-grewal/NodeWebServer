8e709d4d34ffc455f69dc53ca2f7b1e91747d451
Harman Grewal <Harman Grewal>
2017-02-20 18:01:54 +0530

    second

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');    // requiring express
 const hbs  = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 var app = express();   // making new express app
 hbs.registerPartials(__dirname + '/views/partials') // it is for partials .in this i am specifing dericotry where i will use all my handlebars
 app.set('view engine', 'hbs');
@@ -58,7 +59,7 @@ app.get('/about', (req, res) => {
 
   });
 });
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 
 });
